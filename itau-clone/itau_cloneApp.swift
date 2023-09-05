6192ad9241d5cc019745b2da89d846aa059fd204
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2023-09-05 18:14:14 -0300

    Initial Commit

---
+++ itau-clone/itau_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  itau_cloneApp.swift
+//  itau-clone
+//
+//  Created by LUCAS on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct itau_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
